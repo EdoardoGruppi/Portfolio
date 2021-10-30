31d2bbac9294c174379699c8fe1b37950ae1280a
EdoardoGruppi <edoardo.gruppi.20@ucl.ac.uk>
2021-10-30 19:45:05 +0200

    Update

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,37 @@
 export const educationData = [
-    {
-        id: 1,
-        institution: 'Marble Hills Middle School',
-        course: 'Higher Secondary Education',
-        startYear: '2007',
-        endYear: '2015'
-    },
-    {
-        id: 2,
-        institution: 'Somerset School of Science',
-        course: 'Bachelor of Technology',
-        startYear: '2015',
-        endYear: '2019'
-    },
-    {
-        id: 3,
-        institution: 'Hawking University, Cambridge',
-        course: 'Master of Technology',
-        startYear: '2019',
-        endYear: 'Present'
-    },
-]
+  {
+    id: 1,
+    institution: "University College London (UCL)",
+    course: "Master in Integrated Machine Learning Systems",
+    startYear: "2020",
+    endYear: "2021",
+  },
+  {
+    id: 2,
+    institution: "University of Genoa",
+    course: "Bachelor in Electronic Engineering and ICT",
+    startYear: "2017",
+    endYear: "2020",
+  },
+  {
+    id: 3,
+    institution: "ISSUGE Institute of Excellence",
+    course: "Genoa University Superior School",
+    startYear: "2018",
+    endYear: "2019",
+  },
+  {
+    id: 4,
+    institution: "ISICT Institute of Excellence",
+    course: "Institute of Advanced Studies in ICT",
+    startYear: "2017",
+    endYear: "2018",
+  },
+  {
+    id: 5,
+    institution: "Scientific High School G.D.Cassini",
+    course: "Higher Secondary Education",
+    startYear: "2019",
+    endYear: "Present",
+  },
+];
